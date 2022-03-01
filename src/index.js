bd903cd0bf01efadcf427a4cbcb9dca73db4aee2
zboers <100130770+zboers@users.noreply.github.com>
2022-03-01 13:54:33 +0100

    remove showTemperature

--- src/index.js
+++ src/index.js
@@ -114,25 +114,6 @@ function covertToCelcius(event) {
 let celciusLink = document.querySelector("#celcius-link");
 celciusLink.addEventListener("click", covertToCelcius);
 
-function showTemperature(response) {
-  let temperature = Math.round(response.data.main.temp);
-  let heading = document.querySelector("#temperature");
-  let location = document.querySelector("#city-name");
-  let wind = document.querySelector("#windspeed");
-  let description = document.querySelector("#description");
-  let icon = document.querySelector("#icon");
-
-  heading.innerHTML = `${temperature}`;
-  location.innerHTML = response.data.name;
-  wind.innerHTML = response.data.wind.speed;
-  description.innerHTML = response.data.weather[0].description;
-  icon.setAttribute(
-    "src",
-    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
-  );
-  icon.setAttribute("alt", response.data.weather[0].description);
-}
-
 function showPosition(position) {
   let latitude = position.coords.latitude;
   let longitude = position.coords.longitude;
@@ -142,7 +123,7 @@ function showPosition(position) {
   let apiEndpoint = "https://api.openweathermap.org/data/2.5/weather";
   let apiURL = `${apiEndpoint}?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=${units}`;
   console.log(apiURL);
-  axios.get(apiURL).then(showTemperature);
+  axios.get(apiURL).then(displayWeatherCondition);
 }
 
 function getCurrentPosition() {
