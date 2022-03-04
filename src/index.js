8eb0525573507504a81fe1072e154a0958ac2738
zboers <100130770+zboers@users.noreply.github.com>
2022-03-04 18:09:45 +0100

    format-day

--- src/index.js
+++ src/index.js
@@ -49,26 +49,44 @@ if (minutes < 10) {
 let currentTime = document.querySelector("#time");
 currentTime.innerHTML = `${hours}:${minutes}`;
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[day];
+}
+
 function displayForecast(response) {
-  console.log(response.data.daily);
+  let forecast = response.data.daily;
   let forecastElement = document.querySelector("#forecast");
 
   let forecastHTML = `<div class="row">`;
-  let days = ["Tue", "Wed", "Thu", "Fri"];
-  days.forEach(function (day) {
-    forecastHTML =
-      forecastHTML +
-      `
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `
       <div class="col-2">
-       <div class="weather-forecast-date">${day}</div>          
-        <i class="fas fa-cloud-meatball"></i>
-          <br />
+       <div class="weather-forecast-date">${formatDay(
+         forecastDay.dt
+       )}</div>          
+        <img src="http://openweathermap.org/img/wn/${
+          forecastDay.weather[0].icon
+        }@2x.png"
+        alt""
+        width="42"
+        />
           <div class="weather-forecast-temperature">
-            <span class="weather-forecast-temperature-max">12° </span> 
-            <span class="weather-forecast-temperature-min"> 7°</div>
+            <span class="weather-forecast-temperature-max">
+            ${Math.round(forecastDay.temp.max)}° </span> 
+            <span class="weather-forecast-temperature-min">${Math.round(
+              forecastDay.temp.min
+            )}°</span>
         </div>
       </div>
     `;
+    }
   });
 
   forecastHTML = forecastHTML + `</div>`;
